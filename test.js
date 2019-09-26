b2e650d43677d76ee3fa68584561d44809ce33cd
Martyna Michno <mjjmichno@gmail.com>
2019-09-26 10:55:41 +0200

    Lesson 13

--- test.js
+++ test.js
@@ -1,4 +1,19 @@
-//the first alert 
-alert(" Hello, world!");
-// the second alert
-alert("hi again");
+var youLikeMeat = true;
+
+var myNum = 8;
+
+if (youLikeMeat){
+
+document.write("Here is the meaty menu...");
+
+}
+
+if (myNum == 10){
+
+    document.write("my number is equal 10");
+}
+
+else{
+
+    document.write("my number is not equal to 10");
+}
