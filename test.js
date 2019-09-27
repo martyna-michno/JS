8a29f329a5696adb275240392b23dcea2f9b0208
Martyna Michno <mjjmichno@gmail.com>
2019-09-27 19:15:20 +0200

    Lesson 25

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
-var a = "hello";
-var b = 5;
 
-//double negative
-if (!isNaN(a)){
+var myString = 'I\'m a "fun" string';
 
-    console.log("meaning of like is " + (a + b));
 
-}
+console.log(myString.length);
+
+console.log(myString.toUpperCase());
+
+console.log(myString.indexOf("string"));
+
+var string1 = "abc";
+var string2 = "ABC";
+
+console.log(string1.toLowerCase() === string2.toLowerCase());
+ 
+console.log(string1 < string2)
+
+//b jest wyzej niz a, ale A wieksze niz B
