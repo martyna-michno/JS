6d051104fce7467840c24aaca804a7f4fd198ef8
Martyna Michno <mjjmichno@gmail.com>
2019-09-28 10:44:42 +0200

    Lesson 28

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-var myArray = [];
-myArray[0] = 25;
-var myArray = [10, 20, "hi", false
-]
+var myCar = new Car()
+var myString = new String()
+
+myString = "hello"
+myString.length //nie ma nawiasow bo to jest cecha, a nie funkcja
+myString.toUpperCase() //to funkcja - sa nawiasy na koncu
+
+var myString2 = "hi there";
