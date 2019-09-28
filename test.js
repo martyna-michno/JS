2fd34944e5c456b1c0f2f7a773e584bf971a50e4
Martyna Michno <mjjmichno@gmail.com>
2019-09-28 11:40:14 +0200

    Lesson 32

--- test.js
+++ test.js
@@ -1,45 +1,41 @@
+var myDate = new Date();
+ console.log(myDate);
 
 
+ var myPastDate = new Date(1545, 11, 2, 10, 39, 20);
+ var myFutureDate = new Date(2545, 0, 5, 10, 30, 38);
 
 
-var myCar = {
-    maxSpeed: 70, 
-    driver: "Martyna",
-    drive: function(speed, time){
+ console.log(myPastDate);
+ console.log(myFutureDate);
 
-        console.log(speed * time);
-    },
+ var birthday = new Date(1985, 0, 15, 11, 15, 25);
+ var birthday2 = new Date(1985, 0, 15, 11, 15, 25);
+
+ //get the month of the date (0-11)
+ console.log(birthday.getMonth());
+
+ //get the full year (YYYY)
+ console.log(birthday.getFullYear());
+
+ //get the date of the month (1-31)
+ console.log(birthday.getDate());
+
+ //get the day of the week (0-sunday - 6)
+ console.log(birthday.getDay());
+
+ //get the hour of the date (0-23)
+ console.log(birthday.getHours());
+
+ //get the number of milliseconds since 1st Jan 1970
+ console.log(birthday.getTime());
 
 
-    logDriver: function(){
+ if(birthday.getTime() == birthday2.getTime()){
+     
+     console.log("birthdays are equal");
 
-        console.log("driver name is " + this.driver);
-    }
-};
-
-var myArray = new Array();
-var myString = new String();
-
-
-var Car = function(maxSpeed, driver){
-
-this.maxSpeed = maxSpeed;
-this.driver = driver;
-this.drive = function(speed, time){
-
-    console.log(speed * time);
-};
-this.logDriver = function(){
-
-    console.log("driver name is " + this.driver);
-};
-
-} //;? czemu nie ma?
-
-var myCar = new Car(70, "Martyna");
-var myCar2 = new Car(90, "Jagoda");
-var myCar3 = new Car(40, "Karolina");
-var myCar4 = new Car(20, "Asia");
-
-myCar.drive(20, 4);
-myCar3.logDriver();
+ }
+ else{
+     console.log("birthdays are not equal");
+ }
