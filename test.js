467507b16aff82c6b877743202575bb62e4d294b
Martyna Michno <mjjmichno@gmail.com>
2019-09-28 12:52:11 +0200

    Lesson 35

--- test.js
+++ test.js
@@ -1,41 +1,12 @@
-var myDate = new Date();
- console.log(myDate);
+ document.getElementsByClassName("content");
+ /* wpisuje nazwe klas i w konsoli klikam i widze liczbe elementow*/
+ var myContentDivs = document.getElementsByClassName("content");
+
+ myContentDivs
+
+ //wyswietlaja sie znow
 
 
- var myPastDate = new Date(1545, 11, 2, 10, 39, 20);
- var myFutureDate = new Date(2545, 0, 5, 10, 30, 38);
+ var myH2 = myContentDivs[1].getElementsByTagName ("h2");
 
-
- console.log(myPastDate);
- console.log(myFutureDate);
-
- var birthday = new Date(1985, 0, 15, 11, 15, 25);
- var birthday2 = new Date(1985, 0, 15, 11, 15, 25);
-
- //get the month of the date (0-11)
- console.log(birthday.getMonth());
-
- //get the full year (YYYY)
- console.log(birthday.getFullYear());
-
- //get the date of the month (1-31)
- console.log(birthday.getDate());
-
- //get the day of the week (0-sunday - 6)
- console.log(birthday.getDay());
-
- //get the hour of the date (0-23)
- console.log(birthday.getHours());
-
- //get the number of milliseconds since 1st Jan 1970
- console.log(birthday.getTime());
-
-
- if(birthday.getTime() == birthday2.getTime()){
-     
-     console.log("birthdays are equal");
-
- }
- else{
-     console.log("birthdays are not equal");
- }
+ myH2[0].innerHTML = "HEJKA";
