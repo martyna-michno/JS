47b5f29962a1c5da5844c49ff10d3a1b6a8621ad
Martyna Michno <mjjmichno@gmail.com>
2019-09-29 19:01:54 +0200

    Lesson 46

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
-var myForm = document.forms.myForm;
+var myPara = document.getElementById("content").getElementsByTagName("p")[5];
 
-var message = document.getElementById("message");
-
-myForm.onsubmit = function(){
-
-    if(myForm.name.value == ""){
-
-        message.innerHTML = "please enter a name";
-    
-    }
-    else{
-
-        message.innerHTML = "";
-    }
+var myPara2=$("#content p:last-child");
 
 
-};
+myPara2.addClass("test");
+//remove.Class
+
+myPara2.fadeOut();
+
+//fadeIn
+
+myPara2.css({position: "relative", color: "red"})
+
+myPara2.animate({left: "50px"});
